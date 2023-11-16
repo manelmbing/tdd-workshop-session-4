9601cd8c352fec171d2cac7a515f33db33eb021d
Manuel Regino Márquez Bonilla <manuel.regino.marquez.bonilla@ing.com>
2023-11-16 13:21:44 +0100

    First ATDD iteration, POST test GREEN fase completed, added POST endpoint to AccountRestController with fake implementation estrategy.

--- src/main/java/es/ing/tddworkshopsession4/account/AccountRestController.java
+++ src/main/java/es/ing/tddworkshopsession4/account/AccountRestController.java
@@ -1,8 +1,15 @@
 package es.ing.tddworkshopsession4.account;
 
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccountRestController {
 
+    @PostMapping(path = "/accounts", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Account> createAccount(Account account) {
+        return ResponseEntity.badRequest().build();
+    }
 }
