7c8dba5b06933c22dedd9d25ca1936ffd7094421
Manuel Regino Márquez Bonilla <manuel.regino.marquez.bonilla@ing.com>
2023-11-17 01:41:31 +0100

    Second infrastructure TDD iteration, AccountRestCreationService test RED fase completed, added One test case.

--- src/test/java/es/ing/tddworkshopsession4/account/infrastructure/AccountRestCreationServiceTest.java
+++ src/test/java/es/ing/tddworkshopsession4/account/infrastructure/AccountRestCreationServiceTest.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.Test;
 import java.math.BigDecimal;
 import java.security.InvalidParameterException;
 import java.util.ArrayList;
+import java.util.List;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class AccountRestCreationServiceTest {
@@ -17,5 +19,15 @@ class AccountRestCreationServiceTest {
         AccountRestCreationService accountRestCreationService = new AccountRestCreationService();
 
         assertThrows(InvalidParameterException.class, () -> accountRestCreationService.create(account));
+    }
+
+    @Test
+    void shouldReturnCreatedAccountIfAccountHasOneClient() {
+        Account account = new Account(List.of("1"), BigDecimal.ZERO);
+        AccountRestCreationService accountRestCreationService = new AccountRestCreationService();
+
+        Account createdAccount = accountRestCreationService.create(account);
+
+        assertEquals(account, createdAccount);
     }
 }
